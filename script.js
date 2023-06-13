e8bd9c129a6e2c245db1805427c4a9a629fabc34
wiam <98308761+codeswiam@users.noreply.github.com>
2023-06-13 12:21:10 +0100

    Implement changing number of squares option

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 let container = document.querySelector('.container');
+generateGrid(16);
 
-for (let i = 0; i < 16; i++) {
-    let row = document.createElement('div');
-    row.setAttribute('class', 'row');
-    for (let j = 0; j < 16; j++) {
-        let grid = document.createElement('div');
-        grid.setAttribute('class', 'grid');
-        row.appendChild(grid);
+document.querySelector('.change').addEventListener('click' , () => {
+    let number;
+    do {
+        number = prompt("Enter number (1-100) of squares:");
+    } while (number > 100 || number < 1);
+    container.replaceChildren();
+    generateGrid(number);
+});
+
+function generateGrid(number) {
+    for (let i = 0; i < number; i++) {
+        let row = document.createElement('div');
+        row.setAttribute('class', 'row');
+        for (let j = 0; j < number; j++) {
+            let grid = document.createElement('div');
+            grid.setAttribute('class', 'grid');
+            row.appendChild(grid);
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
+    // if we put the following outside this function then coloring the new grid will not work
+    let grids = document.querySelectorAll('.grid');
+    grids.forEach(grid => grid.addEventListener('mouseover', () => {
+        grid.classList.add('colored');
+    }));
 }
 
-let grids = document.querySelectorAll('.grid');
-grids.forEach(grid => grid.addEventListener('mouseover', () => {
-    grid.classList.add('colored');
-}))
